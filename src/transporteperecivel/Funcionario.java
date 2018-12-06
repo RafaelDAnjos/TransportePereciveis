355b49ea21b22e1e7636ea77a3239133681a5de8
NicolasSampaio <nicolas.pereira.sampaio16@gmail.com>
2018-12-06 20:22:31 -0200

    2 relatórios

--- src/transporteperecivel/Funcionario.java
+++ src/transporteperecivel/Funcionario.java
@@ -1,6 +1,10 @@
 package transporteperecivel;
 
 import conexao.BDFuncionario;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
@@ -179,6 +183,115 @@ public class Funcionario {
         }
         
         JOptionPane.showMessageDialog(null, msg, "Funcionarios", 1);
+    }
+    
+    public void relatorioEntrega() {
+        ArrayList<Pedido> pedidos = new ArrayList();
+        ArrayList<Cliente> clientes = new ArrayList();
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/transportePereciveis", "postgres", "123456");
+            //c.setAutoCommit(false);
+            stmt = c.createStatement();
+            
+            ResultSet rs = stmt.executeQuery(" select cliente.id,nomeficticio,dataentrega,numerobandas from cliente inner join _pedido on(cliente.id = _pedido.id);");
+            while (rs.next()) {
+                Pedido pedido = new Pedido();
+                Cliente cliente = new Cliente();
+                
+                cliente.setId(rs.getInt("id"));
+                cliente.setNomeFicticio(rs.getString("nomeficticio"));
+                
+                pedido.setDataDeEntrega(rs.getString("dataentrega"));
+                pedido.setNumeroBandas(rs.getInt("numerobandas"));
+                
+                clientes.add(cliente);
+                pedidos.add(pedido);
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+        for (int i = 0; i < clientes.size(); i++) {
+            System.out.println("ID: " + clientes.get(i).getId() + " Nome: " + clientes.get(i).getNomeFicticio() + " Data Entrega: " + pedidos.get(i).getDataDeEntrega() + " Número Bandas: " + pedidos.get(i).getNumeroBandas());
+        }
+        
+    }
+    
+    public void relatorioTipoVeiculo(String modelo) {
+        List<Automovel> automoveis = new ArrayList();
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/transportePereciveis", "postgres", "123456");
+            //c.setAutoCommit(false);
+            stmt = c.createStatement();
+            
+            ResultSet rs = stmt.executeQuery("select * from automovel where modelo = '" + modelo + "';");
+            while (rs.next()) {
+                Automovel automovel = new Automovel();
+                automovel.setPlaca(rs.getString("placa"));
+                automovel.setIsrefrigerado(rs.getBoolean("isrefrigerado"));
+                automovel.setCarga_maxima(rs.getFloat("carga_maxima"));
+                automovel.setModelo(rs.getString("modelo"));
+                automovel.setFk_cidade_id(rs.getInt("fk_cidade_id"));
+                automovel.setFk_funcionario_idfuncionario(rs.getInt("fk_funcionario_idFuncionario"));
+                automovel.setId(rs.getInt("id"));
+                
+                automoveis.add(automovel);
+                
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+        for (int i = 0; i < automoveis.size(); i++) {
+            System.out.println("Placa: " + automoveis.get(i).getPlaca()
+                    + " Refrigerado: " + automoveis.get(i).getIsrefrigerado()
+                    + " Carga Máxima: " + automoveis.get(i).getCarga_maxima()
+                    + " Modelo: " + automoveis.get(i).getModelo()
+                    + " ID: " + automoveis.get(i).getId()
+                    + " FK_Funcionário: " + automoveis.get(i).getFk_funcionario_idfuncionario()
+                    + " FK_Cidade: " + automoveis.get(i).getFk_cidade_id());
+        }
+    }
+    
+    public void relatorioBandaPorco(int maiorQue){
+        List<BandaPorco> bandas = new ArrayList();
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/transportePereciveis", "postgres", "123456");
+            //c.setAutoCommit(false);
+            stmt = c.createStatement();
+            
+            ResultSet rs = stmt.executeQuery("select * from bandaporco where quantidade >" + maiorQue + ";");
+            while (rs.next()) {
+                BandaPorco banda = new BandaPorco();
+                
+                banda.setDataDeAbate(rs.getString("dataabate"));
+                banda.setId(rs.getInt("id"));
+                banda.setQuantidade(rs.getInt("quantidade"));
+                
+                bandas.add(banda);
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+        for (int i = 0; i < bandas.size(); i++) {
+            System.out.println("Data de abate: " + bandas.get(i).getDataDeAbate() + " Quantidade: " + bandas.get(i).getQuantidade() + " ID: " + bandas.get(i).getId());
+        }
     }
     
 }
